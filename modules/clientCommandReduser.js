8b2f97e7de6c40d94ddfb847193e621b7379dfa8
Maxim Kucheriaviy <maxim-k@i.ua>
2026-04-08 09:55:24 +0300

    serwer rejection tested

--- modules/clientCommandReduser.js
+++ modules/clientCommandReduser.js
@@ -2,36 +2,32 @@ const { getClients } = require("./server");
 const { Room, rooms } = require("./room");
 
 module.exports = (message, from) => {
-  switch (message.command) {
-    case "createRoomWith":
-      let client = getClients().find((item) => item.name === message.username);
-      client.send(
-        JSON.stringify({
-          command: "requestConnectionWith",
-          user: message.from,
-        })
-      );
-      break;
-    case "createRoomAccept":
-      let clientA = getClients().find((item) => item.name === message.username);
-      let clientB = getClients().find(
-        (item) => item.name === message.usernameB
-      );
-      let response = JSON.stringify({
-        command: "roomCreated",
-      });
-      clientA.send(response);
-      clientB.send(response);
-      rooms.push(new Room(clientA, clientB));
-      break;
-    case "createRoomReject":
-      clientA = getClients().find((item) => item.name === message.username);
-      clientB = getClients().find((item) => item.name === message.usernameB);
-      response = JSON.stringify({
-        command: "roomCreationRejected",
-      });
-      clientA.send(response);
-      clientB.send(response);
-      break;
+  if (message.command === "createRoomWith") {
+    const client = getClients().find((item) => item.name === message.username);
+    client.send(
+      JSON.stringify({
+        command: "requestConnectionWith",
+        user: message.from,
+      })
+    );
+  } else if (message.command === "createRoomAccept") {
+    const clientA = getClients().find((item) => item.name === message.username);
+    const clientB = getClients().find(
+      (item) => item.name === message.usernameB
+    );
+    let response = JSON.stringify({
+      command: "roomCreated",
+    });
+    clientA.send(response);
+    clientB.send(response);
+    rooms.push(new Room(clientA, clientB));
+  } else if (message.command === "createRoomReject") {
+    const clientA = getClients().find((item) => item.name === message.username);
+    const clientB = getClients().find((item) => item.name === message.from);
+    response = JSON.stringify({
+      command: "roomCreationRejected",
+    });
+    clientA.send(response);
+    clientB.send(response);
   }
 };
